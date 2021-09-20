da12703575ef2b48c6c7a6971327419977f9cf88
Eder Sampaio <edersampaio@outlook.com.br>
2021-09-20 15:42:25 -0300

    Run eslint fix script

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from '@testing-library/react'
+import App from './App'
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/React JS Authentication Boilerplate/i);
-  expect(linkElement).toBeInTheDocument();
-});
+  render(<App />)
+  const linkElement = screen.getByText(/React JS Authentication Boilerplate/i)
+  expect(linkElement).toBeInTheDocument()
+})
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import React from 'react';
-
-export default function App() {
+export default function App () {
   return (
     <div>
       <h1>React JS Authentication Boilerplate</h1>
     </div>
-  );
+  )
 }
