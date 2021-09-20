33f02abb29b9eba16b8ec63d0c7385510eb1f58e
Eder Sampaio <edersampaio@outlook.com.br>
2021-09-20 17:26:22 -0300

    Import components

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,11 +3,17 @@ import { PrivateRoute } from './PrivateRoute'
 import { PublicRoute } from './PublicRoute'
 import { HybridRoute } from './HybridRoute'
 
+// page components
+import { Login } from '../pages/Login'
+import { Register } from '../pages/Register'
+import { Home } from '../pages/Home'
+import { Users } from '../pages/Users'
+
 export const Routes = () => (
   <Switch>
-    <PublicRoute path="/login" component={() => <h1>Login</h1>} />
-    <HybridRoute path="/register" component={() => <h1>Register</h1>} />
-    <PrivateRoute exact path="/" component={() => <h1>Home</h1>} />
-    <PrivateRoute path="/users" component={() => <h1>Users</h1>} />
+    <PublicRoute path="/login" component={Login} />
+    <HybridRoute path="/register" component={Register} />
+    <PrivateRoute exact path="/" component={Home} />
+    <PrivateRoute path="/users" component={Users} />
   </Switch>
 )
