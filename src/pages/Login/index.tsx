de3c35b634b512d6e3882853e43f26f03e5cc0f3
Eder Sampaio <edersampaio@outlook.com.br>
2021-09-20 18:14:28 -0300

    Add basic form

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,7 +1,64 @@
+import React, { FormEvent, useState } from 'react'
+
+function initialFormFields () {
+  return {
+    email: '',
+    password: ''
+  }
+}
+
 export function Login () {
+  const [fields, setFields] = useState(initialFormFields)
+
+  function handleChange (e: React.ChangeEvent<HTMLInputElement>) {
+    const field = e.target.name
+    const value = e.target.value
+
+    setFields({
+      ...fields,
+      [field]: value
+    })
+  }
+
+  function resetFormFields () {
+    setFields(initialFormFields)
+  }
+
+  function handleSubmit (e: FormEvent) {
+    e.preventDefault()
+
+    console.log(fields)
+
+    resetFormFields()
+  }
+
   return (
     <div>
       <h1>Login</h1>
+
+      <form noValidate onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="">Email</label>
+          <input
+            value={fields.email}
+            type="email"
+            name="email"
+            onChange={handleChange}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="">Password</label>
+          <input
+            value={fields.password}
+            type="password"
+            name="password"
+            onChange={handleChange}
+          />
+        </div>
+
+        <button type="submit">Submit</button>
+      </form>
     </div>
   )
 }
