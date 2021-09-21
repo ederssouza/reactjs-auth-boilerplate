674218327839520332e2729b18651f5dbc77d7dc
Eder Sampaio <edersampaio@outlook.com.br>
2021-09-20 21:05:48 -0300

    Import AuthContext

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,12 +1,24 @@
+import { useContext } from 'react'
 import { Link } from 'react-router-dom'
 
+import { AuthContext } from '../../context/AuthContext'
+
 export function NavBar () {
+  const { isAuthenticated, user, signOut } = useContext(AuthContext)
+
   return (
     <div>
       <Link to="/login">Login</Link>&nbsp;
       <Link to="/register">Register</Link>&nbsp;
       <Link to="/">Home</Link>&nbsp;
       <Link to="/users">Users</Link>
+
+      {isAuthenticated && (
+        <>
+          <span>{user.email}</span>
+          <button onClick={() => signOut()}>Logout</button>
+        </>
+      )}
     </div>
   )
 }
