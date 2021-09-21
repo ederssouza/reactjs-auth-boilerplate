eeb969c8cc789c02d000c5d5142563920d866517
Eder Sampaio <edersampaio@outlook.com.br>
2021-09-21 17:40:13 -0300

    Implement user list

--- src/pages/Users/index.tsx
+++ src/pages/Users/index.tsx
@@ -1,7 +1,30 @@
+import { useEffect, useState } from 'react'
+
+import { api } from '../../services/api'
+
+interface User {
+  id: number
+  name: string
+}
+
 export function Users () {
+  const [users, setUsers] = useState<User[]>([])
+
+  useEffect(() => {
+    api.get('/users')
+      .then(res => setUsers(res.data.users))
+  }, [])
+
   return (
     <div>
       <h1>Users</h1>
+      <ul>
+        {users.map(user => (
+          <li key={user.id}>
+            <strong>ID:</strong> {user.id} <strong>Name:</strong> {user.name}
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
