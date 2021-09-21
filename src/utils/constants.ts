dec99d79ad622e7501c8c536a9a1e6ca7642e57a
Eder Sampaio <edersampaio@outlook.com.br>
2021-09-21 19:28:05 -0300

    Create constants module

---
+++ src/utils/constants.ts
@@ -0,0 +1,4 @@
+// seconds * minutes * hours * days
+export const COOKIE_EXPIRATION_TIME = 30 // 1 minute
+export const COOKIE_TOKEN = 'reactauth.token'
+export const COOKIE_REFRESH_TOKEN = 'reactauth.refreshToken'
