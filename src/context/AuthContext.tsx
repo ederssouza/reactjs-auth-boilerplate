b9af4e87eda91b89827915518034ad1708ac7f16
Eder Sampaio <edersampaio@outlook.com.br>
2021-09-21 19:29:00 -0300

    Redirect to login route when not exists token

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,8 +1,9 @@
 import { setCookie, parseCookies, destroyCookie } from 'nookies'
 import { createContext, ReactNode, useEffect, useState } from 'react'
-import { useHistory } from 'react-router-dom'
+import { useHistory, useLocation } from 'react-router-dom'
 
 import { api } from '../services/api'
+import { COOKIE_EXPIRATION_TIME } from '../utils/constants'
 
 interface User {
   email: string
@@ -31,6 +32,8 @@ export const AuthContext = createContext({} as AuthContextData)
 export function AuthProvider ({ children }: AuthProviderProps) {
   const [user, setUser] = useState<User | null>()
   const history = useHistory()
+  const { pathname } = useLocation()
+
   const { 'reactauth.token': token } = parseCookies()
   const isAuthenticated = Boolean(token)
   const userData = user as User
@@ -40,14 +43,14 @@ export function AuthProvider ({ children }: AuthProviderProps) {
       const response = await api.post('/sessions', { email, password })
       const { token, refreshToken, permissions, roles } = response.data
 
-      setCookie(undefined, 'reactauth.token', token, {
-        maxAge: 60 * 60 * 24 * 30, // 30 days
-        path: '/' // qualquer rota da aplicação terá acesso ao cookie
+      setCookie(null, 'reactauth.token', token, {
+        maxAge: COOKIE_EXPIRATION_TIME,
+        path: '/'
       })
 
-      setCookie(undefined, 'reactauth.refreshToken', refreshToken, {
-        maxAge: 60 * 60 * 24 * 30, // 30 days
-        path: '/' // qualquer rota da aplicação terá acesso ao cookie
+      setCookie(null, 'reactauth.refreshToken', refreshToken, {
+        maxAge: COOKIE_EXPIRATION_TIME,
+        path: '/'
       })
 
       setUser({
@@ -68,6 +71,10 @@ export function AuthProvider ({ children }: AuthProviderProps) {
     setUser(null)
     history.push('/login')
   }
+
+  useEffect(() => {
+    if (!token) signOut()
+  }, [pathname, token])
 
   useEffect(() => {
     const { 'reactauth.token': token } = parseCookies()
