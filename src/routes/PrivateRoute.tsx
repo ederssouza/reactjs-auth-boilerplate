d5cb8ecbb18070fce31a9213b47bd3e1dd5fdc95
Eder Sampaio <edersampaio@outlook.com.br>
2021-09-22 16:45:36 -0300

    Inplement IPrivateRouteProps interface

--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -3,10 +3,14 @@ import { Route, Redirect, RouteProps } from 'react-router-dom'
 
 import { AuthContext } from '../context/AuthContext'
 
-export const PrivateRoute = (props: RouteProps) => {
+interface IPrivateRouteProps extends RouteProps {
+  hasAccess?: boolean
+}
+
+export const PrivateRoute = ({ hasAccess = true, ...rest }: IPrivateRouteProps) => {
   const { isAuthenticated } = useContext(AuthContext)
 
-  return isAuthenticated
-    ? (<Route {...props} />)
-    : (<Redirect to={{ pathname: '/login', state: { from: props.location } }} />)
+  return isAuthenticated && hasAccess
+    ? (<Route {...rest} />)
+    : (<Redirect to={{ pathname: '/login', state: { from: rest.location } }} />)
 }
