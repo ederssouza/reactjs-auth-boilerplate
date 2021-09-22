afbf6cb965cb1cef30f7848be48675beb9586ac5
Eder Sampaio <edersampaio@outlook.com.br>
2021-09-22 10:59:26 -0300

    Rename constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
 // seconds * minutes * hours * days
 export const COOKIE_EXPIRATION_TIME = 60 * 60 * 24 // 1 day
-export const COOKIE_TOKEN = 'reactauth.token'
-export const COOKIE_REFRESH_TOKEN = 'reactauth.refreshToken'
+export const TOKEN_COOKIE = 'reactauth.token'
+export const REFRESH_TOKEN_COOKIE = 'reactauth.refreshToken'
