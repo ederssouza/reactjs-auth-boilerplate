86276d3a142b48fd1df9d6dcde9c6bc289e5d417
Eder Sampaio <edersampaio@outlook.com.br>
2021-09-23 18:46:27 -0300

    Import CanAccess component

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -2,17 +2,24 @@ import { useContext } from 'react'
 import { Link } from 'react-router-dom'
 
 import { AuthContext } from '../../context/AuthContext'
+import { CanAccess } from '../CanAccess'
 
 export function NavBar () {
   const { isAuthenticated, user, signOut } = useContext(AuthContext)
 
   return (
     <div>
-      <Link to="/login">Login</Link>&nbsp;
-      <Link to="/register">Register</Link>&nbsp;
-      <Link to="/">Home</Link>&nbsp;
-      <Link to="/users">Users</Link>&nbsp;
-      <Link to="/metrics">Metrics</Link>
+      <Link to="/login">Login</Link>
+      <Link to="/register">Register</Link>
+      <Link to="/">Home</Link>
+
+      <CanAccess permissions={['users.list']}>
+        <Link to="/users">Users</Link>
+      </CanAccess>
+
+      <CanAccess permissions={['metrics.list']}>
+        <Link to="/metrics">Metrics</Link>
+      </CanAccess>
 
       {isAuthenticated && (
         <>
