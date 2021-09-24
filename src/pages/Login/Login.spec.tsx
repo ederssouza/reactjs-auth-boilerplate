40a748b8e7a715058999c1de3dea47f6a22c750f
Eder Sampaio <edersampaio@outlook.com.br>
2021-09-24 15:00:47 -0300

    Add login tests

--- src/pages/Login/Login.spec.tsx
+++ src/pages/Login/Login.spec.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 
 import { Login } from '.'
 
@@ -8,4 +8,32 @@ describe('Login page component', () => {
     const linkElement = screen.getByText(/Login/i)
     expect(linkElement).toBeInTheDocument()
   })
+
+  it('should have a value on input value in the fields', () => {
+    render(<Login />)
+
+    const $inputEmail = screen.getByTestId('login-input-email') as HTMLInputElement
+    const $inputPassword = screen.getByTestId('login-input-password') as HTMLInputElement
+
+    fireEvent.change($inputEmail, { target: { value: 'email@site.com' } })
+    fireEvent.change($inputPassword, { target: { value: 'senha@123' } })
+
+    expect($inputEmail.value).toEqual('email@site.com')
+    expect($inputPassword.value).toEqual('senha@123')
+  })
+
+  it('should disabled button when submit form', () => {
+    render(<Login />)
+
+    const $form = screen.getByTestId('login-form') as HTMLFormElement
+    const $button = screen.getByTestId('login-submit-button') as HTMLButtonElement
+
+    console.log($button.innerHTML)
+
+    fireEvent.submit($form)
+
+    console.log($button.innerHTML)
+  })
+
+  it.todo('handleSubmit')
 })
