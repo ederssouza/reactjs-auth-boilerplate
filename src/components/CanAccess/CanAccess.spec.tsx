47577d63958c1811ac9417df106a632d43de2151
Eder Sampaio <edersampaio@outlook.com.br>
2021-09-24 17:31:44 -0300

    Add tests on CanAccess component

---
+++ src/components/CanAccess/CanAccess.spec.tsx
@@ -0,0 +1,9 @@
+// import { render, screen } from '@testing-library/react'
+
+// import { CanAccess } from '.'
+
+describe('CanAccess component', () => {
+  it.todo('should render component when user has permission')
+
+  it.todo('should not render component when user has permission')
+})
