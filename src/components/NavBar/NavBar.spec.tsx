938b5151b897a891404f6b8e04132ee9606908b0
Eder Sampaio <edersampaio@outlook.com.br>
2021-09-24 18:42:08 -0300

    Add more tests

--- src/components/NavBar/NavBar.spec.tsx
+++ src/components/NavBar/NavBar.spec.tsx
@@ -1,17 +1,38 @@
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import { BrowserRouter } from 'react-router-dom'
 
 import { NavBar } from '.'
+import { AuthContext } from '../../context/AuthContext'
 
-const NavbarContainer = () => (
-  <BrowserRouter>
-    <NavBar />
-  </BrowserRouter>
-)
+const providerUserUnloggedMock = {
+  signIn: jest.fn(),
+  signOut: jest.fn(),
+  user: null,
+  isAuthenticated: false,
+  loadingUserData: false
+}
+
+const providerUserLoggedMock = {
+  signIn: jest.fn(),
+  signOut: jest.fn(),
+  user: {
+    email: 'email@site.com',
+    permissions: ['users.list', 'metrics.list'],
+    roles: []
+  },
+  isAuthenticated: true,
+  loadingUserData: false
+}
 
 describe('NavBar component', () => {
   it('should render with success', () => {
-    render(<NavbarContainer />)
+    render(
+      <BrowserRouter>
+        <AuthContext.Provider value={providerUserUnloggedMock as any}>
+          <NavBar />
+        </AuthContext.Provider>
+      </BrowserRouter>
+    )
 
     expect(screen.getByText(/Login/)).toBeInTheDocument()
     expect(screen.getByText(/Login/)).toHaveAttribute('href', '/login')
@@ -20,9 +41,31 @@ describe('NavBar component', () => {
     expect(screen.getByText(/Blog/)).toHaveAttribute('href', '/blog')
   })
 
-  it.todo('should not show user email when not is authenticated')
+  it('should show user email when is authenticated', () => {
+    render(
+      <BrowserRouter>
+        <AuthContext.Provider value={providerUserLoggedMock as any}>
+          <NavBar />
+        </AuthContext.Provider>
+      </BrowserRouter>
+    )
 
-  it.todo('should show user email when not is authenticated')
+    expect(screen.getByText(/email@site\.com/)).toBeInTheDocument()
+  })
 
-  it.todo('should logout user on click logout button')
+  it('should logout user on click logout button', () => {
+    render(
+      <BrowserRouter>
+        <AuthContext.Provider value={providerUserLoggedMock as any}>
+          <NavBar />
+        </AuthContext.Provider>
+      </BrowserRouter>
+    )
+
+    const $logoutButton = screen.getByTestId('logout-button')
+
+    fireEvent.click($logoutButton)
+
+    expect(providerUserLoggedMock.signOut).toBeCalledTimes(1)
+  })
 })
