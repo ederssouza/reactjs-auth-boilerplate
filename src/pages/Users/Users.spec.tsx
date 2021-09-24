26e77dd895b04101e7338e37985a127d4cd270ab
Eder Sampaio <edersampaio@outlook.com.br>
2021-09-24 17:33:43 -0300

    Add tests on Users component

--- src/pages/Users/Users.spec.tsx
+++ src/pages/Users/Users.spec.tsx
@@ -1,11 +1,62 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
+import { mocked } from 'ts-jest/utils'
 
 import { Users } from '.'
+import { api } from '../../services/api'
+
+jest.mock('../../services/api')
 
 describe('Users page component', () => {
-  it('should render with success', () => {
+  it('should render list of users', async () => {
+    const getUsersMocked = mocked(api.get)
+    const responseMock = {
+      data: {
+        users: [
+          { id: 1, name: 'User 1', email: 'user1@site.com' },
+          { id: 2, name: 'User 2', email: 'user2@site.com' }
+        ]
+      }
+    }
+
+    getUsersMocked.mockReturnValueOnce({ ...responseMock } as any)
+
     render(<Users />)
-    const linkElement = screen.getByText(/Users/i)
-    expect(linkElement).toBeInTheDocument()
+
+    expect(getUsersMocked).toHaveBeenCalledTimes(1)
+
+    await waitFor(() => {
+      expect(screen.getByText(/User 1/)).toBeInTheDocument()
+      expect(screen.getByText(/User 2/)).toBeInTheDocument()
+    }, { timeout: 1000 })
+
+    expect(getUsersMocked).toHaveReturnedWith({ ...responseMock })
+  })
+
+  it('should render empty list message when request not return payload', async () => {
+    const getUsersMocked = mocked(api.get)
+
+    getUsersMocked.mockReturnValueOnce({ data: {} } as any)
+
+    render(<Users />)
+
+    expect(getUsersMocked).toHaveBeenCalledTimes(1)
+
+    await waitFor(() => {
+      expect(screen.getByText(/empty user list/)).toBeInTheDocument()
+    }, { timeout: 1000 })
+  })
+
+  it('should ', async () => {
+    const getUsersMocked = mocked(api.get)
+
+    getUsersMocked.mockRejectedValueOnce({})
+
+    render(<Users />)
+
+    expect(getUsersMocked).toHaveBeenCalledTimes(1)
+
+    await waitFor(() => {
+      expect(screen.getByText(/empty user list/)).toBeInTheDocument()
+    }, { timeout: 1000 })
   })
 })
