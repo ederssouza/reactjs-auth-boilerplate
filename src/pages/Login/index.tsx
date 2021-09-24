2b3f37d31e4392cfb827b8dc8dcc8f8e0166bbdf
Eder Sampaio <edersampaio@outlook.com.br>
2021-09-24 15:01:16 -0300

    Implement login tests

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,4 +1,4 @@
-import React, { FormEvent, useContext, useState } from 'react'
+import React, { FormEvent, useContext, useEffect, useState } from 'react'
 
 import { AuthContext } from '../../context/AuthContext'
 
@@ -11,6 +11,7 @@ function initialFormValues () {
 
 export function Login () {
   const [values, setValues] = useState(initialFormValues)
+  const [loginRequestStatus, setLoginRequestStatus] = useState('success')
   const { signIn } = useContext(AuthContext)
 
   function handleChange (e: React.ChangeEvent<HTMLInputElement>) {
@@ -23,22 +24,39 @@ export function Login () {
   }
 
   async function handleSubmit (e: FormEvent) {
-    e.preventDefault()
+    setLoginRequestStatus('loading')
 
-    await signIn(values)
+    try {
+      e.preventDefault()
+      await signIn(values)
+      setLoginRequestStatus('success')
+    } catch (error) {
+      setLoginRequestStatus('error')
+    }
   }
+
+  useEffect(() => {
+    // clean the function to fix memory leak
+    return () => setLoginRequestStatus('success')
+  }, [])
 
   return (
     <div>
       <h1>Login</h1>
 
-      <form noValidate onSubmit={handleSubmit}>
+      <form
+        noValidate
+        data-testid="login-form"
+        onSubmit={handleSubmit}
+      >
         <div>
           <label htmlFor="">Email</label>
           <input
             value={values.email}
             type="email"
             name="email"
+            data-testid="login-input-email"
+            disabled={loginRequestStatus === 'loading'}
             onChange={handleChange}
           />
         </div>
@@ -49,11 +67,19 @@ export function Login () {
             value={values.password}
             type="password"
             name="password"
+            data-testid="login-input-password"
+            disabled={loginRequestStatus === 'loading'}
             onChange={handleChange}
           />
         </div>
 
-        <button type="submit">Submit</button>
+        <button
+          type="submit"
+          data-testid="login-submit-button"
+          disabled={loginRequestStatus === 'loading'}
+        >
+          {loginRequestStatus === 'loading' ? 'Loading...' : 'Submit'}
+        </button>
       </form>
     </div>
   )
