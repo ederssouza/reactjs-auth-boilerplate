1cf2d5d29bf417fe7e08bcc87d9216357d1f2cc5
Eder Sampaio <edersampaio@outlook.com.br>
2021-09-25 10:59:45 -0300

    Remove ThemeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,13 @@ import { BrowserRouter } from 'react-router-dom'
 
 import { NavBar } from './components/NavBar'
 import { AuthProvider } from './context/AuthContext'
-import { ThemeProvider } from './context/ThemeContext'
 import { Routes } from './routes'
 
 const App = () => (
   <BrowserRouter>
     <AuthProvider>
       <NavBar />
-
-      <ThemeProvider>
-        <Routes />
-      </ThemeProvider>
+      <Routes />
     </AuthProvider>
   </BrowserRouter>
 )
