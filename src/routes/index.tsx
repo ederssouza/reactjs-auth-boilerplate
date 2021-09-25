c26aeb5323fca7bc8cfd75f527029a9f2dc24457
Eder Sampaio <edersampaio@outlook.com.br>
2021-09-25 10:59:20 -0300

    Remove blog route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,5 @@
 import { Switch } from 'react-router-dom'
 
-import { Blog } from '../pages/Blog'
 import { Home } from '../pages/Home'
 import { Login } from '../pages/Login'
 import { Metrics } from '../pages/Metrics'
@@ -12,25 +11,20 @@ import { PublicRoute } from './PublicRoute'
 
 export const Routes = () => (
   <Switch>
+    <PrivateRoute
+      exact
+      path="/"
+      component={Home}
+    />
+
     <PublicRoute
       path="/login"
       component={Login}
     />
 
     <HybridRoute
-      path="/blog"
-      component={Blog}
-    />
-
-    <HybridRoute
       path="/register"
       component={Register}
-    />
-
-    <PrivateRoute
-      exact
-      path="/"
-      component={Home}
     />
 
     <PrivateRoute
