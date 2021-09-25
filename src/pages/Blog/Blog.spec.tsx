8c4be7758d2545e6dd26d67a2bb1be1b443233c3
Eder Sampaio <edersampaio@outlook.com.br>
2021-09-25 09:20:53 -0300

    Remove dead code

--- src/pages/Blog/Blog.spec.tsx
+++ src/pages/Blog/Blog.spec.tsx
@@ -1,5 +1,4 @@
 import { fireEvent, render, screen } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
 
 import { Blog } from '.'
 import { ThemeContext } from '../../context/ThemeContext'
