792f8dcb333f5a2b51c21c6d4831e21c39bc3a02
Eder Sampaio <edersampaio@outlook.com.br>
2021-09-25 10:34:38 -0300

    Add more tests

--- src/context/AuthContext/AuthContext.spec.tsx
+++ src/context/AuthContext/AuthContext.spec.tsx
@@ -5,6 +5,8 @@ import { AuthProvider } from '.'
 import { Profile } from '../../pages/Profile'
 import { api } from '../../services/api'
 
+jest.mock('../../services/api')
+
 jest.mock('react-router-dom', () => ({
   useHistory: () => ({
     push: jest.fn()
@@ -13,8 +15,6 @@ jest.mock('react-router-dom', () => ({
     pathname: '/'
   })
 }))
-
-jest.mock('../../services/api')
 
 describe('AuthProvider', () => {
   it('should dispatch signIn function when invoked and return valid response', async () => {
@@ -58,6 +58,46 @@ describe('AuthProvider', () => {
 
     const $signInButton = screen.getByTestId('signin-button')
     fireEvent.click($signInButton)
+
+    await waitFor(() => {
+      expect(signInMocked).toHaveBeenCalledTimes(1)
+    }, { timeout: 1000 })
+  })
+
+  it('should return valid paylod on make `/me`', async () => {
+    const signInMocked = mocked(api.get)
+    const responseMock = {
+      data: {
+        email: 'admin@site.com',
+        permissions: ['users.list', 'users.create', 'metrics.list'],
+        roles: ['administrator']
+      }
+    }
+
+    signInMocked.mockReturnValueOnce({ ...responseMock } as any)
+
+    render(
+      <AuthProvider>
+        <Profile />
+      </AuthProvider>
+    )
+
+    await waitFor(() => {
+      expect(signInMocked).toHaveBeenCalledTimes(1)
+      expect(signInMocked).toHaveReturnedWith({ ...responseMock })
+    }, { timeout: 1000 })
+  })
+
+  it('should return erro when `/me` request is invalid', async () => {
+    const signInMocked = mocked(api.get)
+
+    signInMocked.mockRejectedValueOnce({})
+
+    render(
+      <AuthProvider>
+        <Profile />
+      </AuthProvider>
+    )
 
     await waitFor(() => {
       expect(signInMocked).toHaveBeenCalledTimes(1)
