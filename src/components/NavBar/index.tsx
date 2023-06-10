622939efb9b0e4b1b5a26973fe7216f52cd00f77
Eder Sampaio <edersampaio@outlook.com.br>
2023-06-10 18:28:35 -0300

    Change AuthContext import path

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,7 +1,7 @@
 import { useContext } from 'react'
 import { Link } from 'react-router-dom'
 
-import { AuthContext } from '../../context/AuthContext'
+import { AuthContext } from '../../contexts/AuthContext'
 import { CanAccess } from '../CanAccess'
 
 export function NavBar () {
