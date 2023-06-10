c069ee47f80bf3e54bc3983c13940361f7b518b1
Eder Sampaio <edersampaio@outlook.com.br>
2023-06-10 18:30:14 -0300

    Implement AuthProvider component

---
+++ src/providers/AuthProvider/index.tsx
@@ -0,0 +1,85 @@
+import { AxiosError } from 'axios'
+import { ReactNode, useEffect, useState } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
+
+import { AuthContext, SignInCredentials, User } from '../../contexts/AuthContext'
+import { api } from '../../services/api'
+import { setAuthorizationHeader } from '../../services/interceptors'
+import { createTokenCookies, getToken, removeTokenCookies } from '../../utils/tokenCookies'
+
+type AuthProviderProps = {
+  children: ReactNode
+}
+
+export function AuthProvider ({ children }: AuthProviderProps) {
+  const [user, setUser] = useState<User | null>()
+  const [loadingUserData, setLoadingUserData] = useState(true)
+  const navigate = useNavigate()
+  const { pathname } = useLocation()
+  const token = getToken()
+  const isAuthenticated = Boolean(token)
+  const userData = user as User
+
+  async function signIn ({ email, password }: SignInCredentials) {
+    try {
+      const response = await api.post('/sessions', { email, password })
+      const { token, refreshToken, permissions, roles } = response.data
+
+      createTokenCookies(token, refreshToken)
+      setUser({ email, permissions, roles })
+      setAuthorizationHeader(api.defaults, token)
+    } catch (error) {
+      const err = error as AxiosError
+      return err
+    }
+  }
+
+  function signOut (pathname = '/login') {
+    removeTokenCookies()
+    setUser(null)
+    setLoadingUserData(false)
+    navigate(pathname)
+  }
+
+  useEffect(() => {
+    if (!token) signOut(pathname)
+  }, [pathname, token])
+
+  useEffect(() => {
+    const token = getToken()
+
+    async function getUserData () {
+      setLoadingUserData(true)
+
+      try {
+        const response = await api.get('/me')
+
+        if (response?.data) {
+          const { email, permissions, roles } = response.data
+          setUser({ email, permissions, roles })
+        }
+      } catch (error) {
+        signOut()
+      }
+
+      setLoadingUserData(false)
+    }
+
+    if (token) {
+      setAuthorizationHeader(api.defaults, token)
+      getUserData()
+    }
+  }, [])
+
+  return (
+    <AuthContext.Provider value={{
+      isAuthenticated,
+      user: userData,
+      loadingUserData,
+      signIn,
+      signOut
+    }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
