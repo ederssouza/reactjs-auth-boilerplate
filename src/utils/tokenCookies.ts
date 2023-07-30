750c2b9ab93e13c79a201fed65341efca53d9226
Eder Sampaio <edersampaio@outlook.com.br>
2023-07-30 18:28:20 -0300

    feat: change functions signature

--- src/utils/tokenCookies.ts
+++ src/utils/tokenCookies.ts
@@ -1,30 +1,45 @@
 import { destroyCookie, parseCookies, setCookie } from 'nookies'
+import {
+  COOKIE_EXPIRATION_TIME,
+  REFRESH_TOKEN_COOKIE,
+  TOKEN_COOKIE
+} from '@/utils'
 
-import { COOKIE_EXPIRATION_TIME, REFRESH_TOKEN_COOKIE, TOKEN_COOKIE } from '../utils/constants'
-
-export function createTokenCookies (token: string, refreshToken: string) {
-  setCookie(null, TOKEN_COOKIE, token, {
-    maxAge: COOKIE_EXPIRATION_TIME,
-    path: '/'
-  })
-
-  setCookie(null, REFRESH_TOKEN_COOKIE, refreshToken, {
-    maxAge: COOKIE_EXPIRATION_TIME,
-    path: '/'
-  })
+type CreateTokenCookiesParams = {
+  token?: string
+  refreshToken?: string
 }
 
-export function removeTokenCookies () {
+export function createTokenCookies({
+  token,
+  refreshToken
+}: CreateTokenCookiesParams) {
+  if (token) {
+    setCookie(null, TOKEN_COOKIE, token, {
+      maxAge: COOKIE_EXPIRATION_TIME,
+      path: '/'
+    })
+  }
+
+  if (refreshToken) {
+    setCookie(null, REFRESH_TOKEN_COOKIE, refreshToken, {
+      maxAge: COOKIE_EXPIRATION_TIME,
+      path: '/'
+    })
+  }
+}
+
+export function removeTokenSession() {
   destroyCookie(null, TOKEN_COOKIE)
   destroyCookie(null, REFRESH_TOKEN_COOKIE)
 }
 
-export function getToken () {
+export function getToken() {
   const cookies = parseCookies()
   return cookies[TOKEN_COOKIE]
 }
 
-export function getRefreshToken () {
+export function getRefreshToken() {
   const cookies = parseCookies()
   return cookies[REFRESH_TOKEN_COOKIE]
 }
